26fde015633a0ea64926ce13b455c5f4dbd16f60
aRakkolainen <aino.rakkolainen@gmail.com>
2024-11-11 11:15:39 +0200

    Configuration for database connection for Oulu

--- purrfectMatchApp/db/ProvinceOfOuluDB.js
+++ purrfectMatchApp/db/ProvinceOfOuluDB.js
@@ -1,2 +1,17 @@
 //Connection for database in Pronvince of Oulu (Oulun lääni)
+// Database connection is based on this website tutorial: https://medium.com/@eslmzadpc13/how-to-connect-a-postgres-database-to-express-a-step-by-step-guide-b2fffeb8aeac
+//Note: The port might need to be changed to match your postgreSQL settings
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'testUser',
+    password: 'x', 
+    host: 'localhost', 
+    port: 5433, 
+    database: 'ProvinceOfOuluDB'
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params)
+}
 
