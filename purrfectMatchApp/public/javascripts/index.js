55b6f9c996c7eb633439eb58515f2f7894e07fe7
me mee <me@email.com>
2024-11-17 13:39:18 +0200

    User/Admin view

--- purrfectMatchApp/public/javascripts/index.js
+++ purrfectMatchApp/public/javascripts/index.js
@@ -6,8 +6,48 @@ const southernFinlandProvinceBtn = document.getElementById("SouthernFinlandProvi
 const provinceOfOuluBtn = document.getElementById("OuluProvinceBtn");
 const easternFinlandProvinceBtn = document.getElementById("EasternFinlandProvinceBtn");
 
+/*function showAdminView () {
+    document.getElementById('openAdmin').addEventListener('click', () => {
+    const userView = document.getElementById('user-view');
+    const adminView = document.getElementById('admin-view');
 
+    adminView.style.display = 'block';
+    userView.style.display = 'none';
 
+    });
+}
+
+showAdminView();
+
+function showUserView () {
+    document.getElementById('openUser').addEventListener('click', () => {
+    const userView = document.getElementById('user-view');
+    const adminView = document.getElementById('admin-view');
+
+    adminView.style.display = 'none';
+    userView.style.display = 'block';
+
+    });
+}
+
+showUserView();*/
+
+function toggleView(showAdmin) {
+    const userView = document.getElementById('user-view');
+    const adminView = document.getElementById('admin-view');
+
+    if (showAdmin) {
+        adminView.style.display = 'block';
+        userView.style.display = 'none';
+    } else {
+        adminView.style.display = 'none';
+        userView.style.display = 'block';
+    }
+}
+
+// Lisätään kuuntelijat
+document.getElementById('openAdmin').addEventListener('click', () => toggleView(true));
+document.getElementById('openUser').addEventListener('click', () => toggleView(false));
 
 
 
--- purrfectMatchApp/routes/users.js
+++ purrfectMatchApp/routes/users.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/data', (req, res) =>{
+  if (req.query.view === 'admin') {
+      res.json ({ data: 'Admin-data'});
+  }
+  else {
+    res.json ({data: 'user-data'});
+  }
+})
+
 module.exports = router;
