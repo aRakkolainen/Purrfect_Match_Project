4095614081929356589369f8c1b77c78c9e1b1af
aRakkolainen <aino.rakkolainen@gmail.com>
2024-11-26 19:28:23 +0200

    Added pagination but it is not yet correctly functional

--- purrfectMatchApp/public/javascripts/rescueCentersAndAnimals.js
+++ purrfectMatchApp/public/javascripts/rescueCentersAndAnimals.js
@@ -1,35 +1,45 @@
+/*
+Sources:
+1) Source for implementing dynamic pagination: https://stackoverflow.com/questions/76086100/how-to-create-pagination-function-for-dynamically-created-table
+
+
+*/
+
+
 const showAllRescueCenters = document.getElementById("allProvincesOption");
 const showOnlySouthernFinlandCenters = document.getElementById("southernFinlandProvinceOption");
 const showOnlyEasternFinlandCenters = document.getElementById("easternFinlandProvinceOption");
 const showOnlyProvinceOfOuluCenters = document.getElementById("provinceOfOuluOption");
 const filterBtn = document.getElementById("filterCentersBtn");
-
+const backToTopBtn = document.getElementById("backToTopBtn");
+const paginationLabel = document.getElementById("paginationLabel");
 const animalCardsList = document.getElementById("rescueCenterAnimals");
+const cardsPerPage = 10;
 
 window.onload = async () => {
     filterBtn.addEventListener("click", async () => {
         console.log("Filtering based on your choice..");
         clearAnimalsCards();
         if(showAllRescueCenters.checked) {
-            console.log("Showing all rescue centers and their contacts persons..");
+            console.log("Showing all rescue centers and their animals..");
             let url = 'http://localhost:3000/list/rescueCenters/animals/all';
             await fetchAndFillRescueAnimalsList(url);
         }
 
         if(showOnlySouthernFinlandCenters.checked) {
-            console.log("Showing rescue centers and their contacts persons in Southern Finland");
+            console.log("Showing rescue centers and their animals in Southern Finland");
             let url = 'http://localhost:3000/list/rescueCenters/animals/southernFinlandProvince';
             await fetchAndFillRescueAnimalsList(url);
         }
 
         if(showOnlyEasternFinlandCenters.checked) {
-            console.log("Showing rescue centers and their contacts persons in Eastern Finland");
+            console.log("Showing rescue centers and their animals in Eastern Finland");
             let url = 'http://localhost:3000/list/rescueCenters/animals/easternFinlandProvince';
             await fetchAndFillRescueAnimalsList(url);
         }
 
         if(showOnlyProvinceOfOuluCenters.checked) {
-            console.log("Showing rescue centers and their contacts persons in Province of Oulu");
+            console.log("Showing rescue centers and their animals in Province of Oulu");
             let url = 'http://localhost:3000/list/rescueCenters/animals/provinceOfOulu';
             await fetchAndFillRescueAnimalsList(url);
         }
@@ -38,24 +48,96 @@ window.onload = async () => {
 async function fetchAndFillRescueAnimalsList(url) {
     let response = await fetchRescueCentersData(url);
     let rescueCenterAnimalsList = response.rescueCenterAnimals;
-    fillRescueCenterAnimalCards(rescueCenterAnimalsList);
+    //Source 1 was used for this pagination part
+    const pageCount = calculatePageCount(rescueCenterAnimalsList.length, cardsPerPage);
+    const remainder = calculateRemainder(rescueCenterAnimalsList.length, cardsPerPage);
+    if(cardsPerPage > rescueCenterAnimalsList.length) {
+        clearAnimalsCards();
+        fillRescueCenterAnimalCards(rescueCenterAnimalsList, 0, rescueCenterAnimalsList.length);
+    } else {
+        clearAnimalsCards();
+        fillRescueCenterAnimalCards(rescueCenterAnimalsList, 0, cardsPerPage);
+    }
+
+    if(pageCount > 1) {
+        let pagination = document.getElementById("pagination");
+        pagination.setAttribute("class", "pagination");
+        for (let j = 0; j < pageCount - 1; j++) {
+            let button = document.createElement("button");
+            button.setAttribute("class", "waves-light btn");
+            button.innerText = parseInt(j+1);
+            button.addEventListener("click", () => {
+                clearAnimalsCards();
+                fillRescueCenterAnimalCards(rescueCenterAnimalsList, j*cardsPerPage, j*cardsPerPage+cardsPerPage);
+            })
+            pagination.appendChild(button);
+          }
+          // Add the last page button which might have less cards
+          if (remainder !== 0) {
+
+            let button = document.createElement("button");
+            
+            button.innerText = pageCount;
+            button.setAttribute("class", "waves-light btn");
+            button.addEventListener("click", () => {
+                clearAnimalsCards();
+                fillRescueCenterAnimalCards(rescueCenterAnimalsList, pageCount-1*cardsPerPage, (pageCount-1)*cardsPerPage+remainder);
+            })
+            pagination.appendChild(button);
+          } else {
+            let button = document.createElement("button");
+            button.setAttribute("class", "waves-light btn");
+            button.innerText = pageCount;
+            button.addEventListener("click", () => {
+                clearAnimalsCards();
+                fillRescueCenterAnimalCards(rescueCenterAnimalsList, pageCount-1*cardsPerPage, (pageCount-1)*cardsPerPage+cardsPerPage);
+            });
+          }
+
+          backToTopBtn.setAttribute("class", "visible");
+          paginationLabel.setAttribute("class", "visible");
+          backToTopBtn.addEventListener("click", () => {
+                document.documentElement.scrollTop = 0;
+          })
+          
+    }
+
+
+
 }
 async function fetchRescueCentersData(url) {
     let result = await fetch(url);
     let rescueCenterAnimalsList = await result.json();
     return rescueCenterAnimalsList;
 }
+//1)
+function calculatePageCount(totalAnimalCards, cardsPerPage) {
+    return Math.ceil(totalAnimalCards/cardsPerPage);
+}
+//1)
+function calculateRemainder(totalAnimalCards, cardsPerPage) {
+    return totalAnimalCards - Math.floor(totalAnimalCards/ cardsPerPage) * cardsPerPage;
+}
 
-function fillRescueCenterAnimalCards(rescueCenterAnimalsList) {
-    if(rescueCenterAnimalsList && rescueCenterAnimalsList.length > 0) {
+
+
+
+function fillRescueCenterAnimalCards(rescueCenterAnimalsList, startIndex, endIndex) {
+    //if(rescueCenterAnimalsList && rescueCenterAnimalsList.length > 0) {
         let id=0;
-
-        rescueCenterAnimalsList.forEach(animal => {
+        for (let i=startIndex; i < endIndex; i++) {
+            let animal = rescueCenterAnimalsList[i];
             let item = createRescueCenterListItem(animal, id);
             animalCardsList.appendChild(item);
             id++;
-        });
-    }
+        }
+        /* rescueCenterAnimalsList.forEach(animal => {
+            while(id > pageCount) {
+                let item = createRescueCenterListItem(animal, id);
+                animalCardsList.appendChild(item);
+                id++;
+            }
+        }); */
 }
 //Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomIndex(max) {
